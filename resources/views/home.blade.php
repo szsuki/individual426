aa8388bf25a3564688fbd25cf106dd34708dd0ee
szsuki <csnara102@gmail.com>
2024-12-04 11:52:01 +0900

    24120410

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -89,7 +89,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <style>
         /* 新着商品カード全体のサイズ調整 */
-        .card mt-4 {
+        .card.mt-4 {
             max-width: 250px; /* カードの最大幅 */
        
         }
