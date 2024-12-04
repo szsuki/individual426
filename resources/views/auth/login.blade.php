619175e2c9dfd618893db325fdc1d7c36ececb61
szsuki <csnara102@gmail.com>
2024-12-04 23:39:51 +0900

    2412053

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -82,7 +82,7 @@
 
 @section('css')
     <style>
-        div.col-md-8{
+        .card-body {
             width: 400px !important;  /* 横幅を狭くする */
             height: 500px !important; /* 高さを縦長に調整 */
         }
